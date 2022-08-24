21ec5e62e2544faa637e6ba569b7d2920519e09a
Romstrich <roman.motrich@yandex.ru>
2022-08-24 05:12:32 +0300

    Готово

--- descriptors.py
+++ descriptors.py
@@ -5,9 +5,17 @@
 '''
 
 class Port:
+    def __set_name__(self, owner, name):
+        # print(owner)
+        # print(name)
+        self.name = name
+# примеры с https://habr.com/ru/post/137415/
+
+
     def __set__(self, instance, value):
-        print('установка порта')
-        print(instance)
-        print(value)
-        print(self.name)
-        print(instance.__dict__)
+        # print('установка порта')
+        # print(instance)
+        # print(value)
+        # print(self.name)
+        # print(instance.__dict__)
+        instance.__dict__[self.name]=value
